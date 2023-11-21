fed213441f15e42cadcf36a3262f5a2ea05544d5
kimberleydaniel56 <148758257+kimberleydaniel56@users.noreply.github.com>
2023-11-21 16:16:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 
 console.log(chalk.red('Nadir'));
-console.log(chalk.green('Soumia));
+console.log(chalk.green('Soumia'));
 console.log(chalk.blue('Sylvain'));
 console.log(chalk.yellow('Arthur'));
